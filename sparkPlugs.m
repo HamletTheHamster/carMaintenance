94e0d9dc6b5070cb163608f8d742aa3253db1b8e
Joel N. Johnson <joel.johnson675@gmail.com>
2014-09-18 11:19:00 -0700

    Create sparkPlugs.m

---
+++ sparkPlugs.m
@@ -0,0 +1,68 @@
+function [ yn ] = spark_plugs( cm )
+%This function takes current mileage as input and determines whether a 
+% spark plug change is required on the next oil change, based on
+% a schedule of every 75,000 miles. If the plug change is due, it prints
+% "-Spark Plugs" among a list of any other required services on that oil 
+% change.
+%
+%   cm = current mileage
+%   ocs = oil change schedule
+%   yn = yes/no
+%
+%   Coded by: Joel N. Johnson
+%---------------------------------------------------------------------
+
+ocs = zeros(20,1);                  %preallocates 20x1 array with zeros
+
+for j = 1:20                        %assigns mileage schedule to array
+    k = j - 1;
+    ocs(j) = 40400 + (7500 * k);
+end
+
+if cm <= ocs(1)                     %Assigns 1 to yn when a plug change
+    yn = 0;                         % is due and assigns 0 to yn when one
+elseif cm <= ocs(2)                 % is not due.
+        yn = 0;                
+elseif cm <= ocs(3)                 
+        yn = 0;                
+elseif cm <= ocs(4)                
+        yn = 0;
+elseif cm <= ocs(5)
+        yn = 0;
+elseif cm <= ocs(6)
+        yn = 1;
+elseif cm <= ocs(7)
+        yn = 0;
+elseif cm <= ocs(8)
+        yn = 0;
+elseif cm <= ocs(9)
+        yn = 0;
+elseif cm <= ocs(10)
+        yn = 0;
+elseif cm <= ocs(11)
+        yn = 0;
+elseif cm <= ocs(12)
+        yn = 1;
+elseif cm <= ocs(13)
+        yn = 0;
+elseif cm <= ocs(14)
+        yn = 0;
+elseif cm <= ocs(15)
+        yn = 0;
+elseif cm <= ocs(16)
+        yn = 1;
+elseif cm <= ocs(17)
+        yn = 0;
+elseif cm <= ocs(18)
+        yn = 0;
+elseif cm <= ocs(19)
+        yn = 0;
+elseif cm <= ocs(20)
+        yn = 1;
+end
+
+if yn == 1
+    fprintf('   -Spark Plugs\n')    %Prints "-Spark Plugs" among a
+end                                 % list of any other services due.
+
+end
